dea502882926fbaf5b37b90fdc14d8d1e5dccb7c
PyroKsen <maxmax11219@yandex.ru>
2024-09-13 12:07:10 +0300

    sdf

--- src/setters/set-hotel.js
+++ src/setters/set-hotel.js
@@ -1,26 +1,38 @@
 import updater from "../file-updater.js";
 import getId from '../getters/get-id.js'
-import readlinkSync from 'readline-sync';
+import readlineSync from 'readline-sync';
 import reader from "../file-reader.js";
 import setRegion from '../setters/set-regions.js'
 
-const newHotel = readlinkSync.question('New hotel: ').toLowerCase().trim()
-const id = getId('data/hotels.csv')
-const newRegion = readlinkSync.question(`Hotel's region: `).toLowerCase().trim()
-const pustota = []
+const setHotel = () => {
+    const newHotel = readlineSync.question('New hotel: ').toLowerCase().trim()
+    const id = getId('data/hotels.csv')
+    const newRegion = readlineSync.question(`Hotel's region: `).toLowerCase().trim()
+    const regions = reader('data/regions.csv').split('\n')
+    const regionId = regions.filter((string) => {
+        const [, name] = string.split(';')
+        return name === newRegion ? true : false
+    })
 
-const regions = reader('data/regions.csv').split('\n')
-const regionId = regions.filter((string) => {
-    const [id, name] = string.split(';')
-    console.log(id, name)
-    return name === newRegion ? true : false
-})
+    let regId;
 
-if (regionId === pustota) {
-    
+    if (regionId[0] === undefined) {
+        console.log('ты ввёл какойто CrIngE')
+        const markToAdd = readlineSync.question('хочешь добавить новый регион? [y/n]: ').toLowerCase().trim()
+        if (markToAdd === 'y') {
+            regId = setRegion(newRegion)
+        } else {
+            return false
+        }
+        
+    } else {
+        regId = regionId.at(0).split(';').at(0)
+    }
+
+    updater('data/hotels.csv', `\n${id};${newHotel};${regId}`)
+    return true
 }
 
-// (string) => string.split(';').at(1) === newRegion
+setHotel()
 
-console.log(regionId)
-updater('data/hotels.csv', `\n${id};${newHotel};${regionId}`)
+export default setHotel
--- src/setters/set-regions.js
+++ src/setters/set-regions.js
@@ -2,11 +2,16 @@ import updater from "../file-updater.js";
 import getId from '../getters/get-id.js'
 import readlinkSync from 'readline-sync';
 
-const setRegion = () => {
-    const newRegion = readlinkSync.question('New region: ').toLocaleLowerCase().trim()
+const setRegion = (name = '') => {
+    let newRegion;
+    if (name === '') {
+        newRegion = readlinkSync.question('New region: ').toLocaleLowerCase().trim()
+    } else {
+        newRegion = name
+    }
     const id = getId('data/regions.csv')
-
     updater('data/regions.csv', `\n${id};${newRegion}`)
+    return id
 }
 
 export default setRegion
